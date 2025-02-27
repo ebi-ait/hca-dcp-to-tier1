9f971c2f6c422cde1245510dba0c85bebec4e2e1
Arsenios Chatzigeorgiou <49944135+arschat@users.noreply.github.com>
2025-02-27 23:37:00 +0000

    Added arguments in run_bionetwork

--- run_bionetwork.py
+++ run_bionetwork.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 
 import pandas as pd
@@ -15,9 +16,13 @@ def define_parser():
                         required=True, help='Name of the bionetwork to process')
     parser.add_argument('--csv', '-c', action='store', dest='csv', type=str, 
                         required=False, default='data/bionetworks.csv', help='Path to bionetwork CSV file')
+    parser.add_argument('--group_field', '-g', action='store', dest='group_field', type=str,
+                        required=False, default=GROUP_FIELD, help='DCP field to group output with')
+    parser.add_argument('--denormalised', '-d', action='store_true', dest='denormalised',
+                        required=False, default=DENORMALISED, help='use the denormalised flat file instead of the grouped one')
     return parser
 
-def main(csv, bionetwork):
+def main(csv, bionetwork, group_field, denormalised):
     df = pd.read_csv(csv)
     xlsx_files = df.loc[df['bionetwork'] == bionetwork.lower(), 'spreadsheet'].tolist()
     for xlsx_file in xlsx_files:
@@ -25,8 +30,8 @@ def main(csv, bionetwork):
             print(f"File {xlsx_file} not found in {INPUT_DIR}")
             continue
         print(f"=====Processing {xlsx_file}=====")
-        dcp_to_tier1(xlsx_file, INPUT_DIR, FLAT_DIR, OUTPUT_DIR, GROUP_FIELD, DENORMALISED)
+        dcp_to_tier1(xlsx_file, INPUT_DIR, FLAT_DIR, OUTPUT_DIR, group_field, denormalised)
 
 if __name__ == '__main__':
     args = define_parser().parse_args()
-    main(args.csv, args.bionetwork)
+    main(args.csv, args.bionetwork, args.group_field, args.denormalised)
