fa6374a460ad9965c5d07c22db3b566e4aae9b6d
Arsenios Chatzigeorgiou <49944135+arschat@users.noreply.github.com>
2025-03-03 21:26:53 +0000

    Added zip functionality for run_bionetworks.py

--- run_bionetwork.py
+++ run_bionetwork.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import zipfile
 
 import pandas as pd
 from dcp_to_tier1 import main as dcp_to_tier1
@@ -22,6 +23,22 @@ def define_parser():
                         required=False, default=DENORMALISED, help='use the denormalised flat file instead of the grouped one')
     return parser
 
+def make_zipfile(input_filenames:list, output_filename:str):
+    with zipfile.ZipFile(output_filename, "w") as zip_file:
+        for filename in input_filenames:
+            zip_file.write(filename, arcname=os.path.basename(filename))
+    print(f"Zip file created at {output_filename}")
+
+def select_zip_files(xlsx_files, denormalised):
+    tier1_files = []
+    extensions = [f'_denormalised_tier1.{format}' if denormalised else f'_tier1.{format}' for format in ['csv', 'xlsx']]
+    for extension in extensions:
+        for xlsx_file in xlsx_files:
+            output_file = f"{OUTPUT_DIR}/{xlsx_file.replace('.xlsx', extension)}"
+            if os.path.exists(output_file):
+                tier1_files.append(output_file)
+    return tier1_files
+
 def main(csv, bionetwork, group_field, denormalised):
     df = pd.read_csv(csv)
     xlsx_files = df.loc[df['bionetwork'] == bionetwork.lower(), 'spreadsheet'].tolist()
@@ -31,6 +48,11 @@ def main(csv, bionetwork, group_field, denormalised):
             continue
         print(f"=====Processing {xlsx_file}=====")
         dcp_to_tier1(xlsx_file, INPUT_DIR, FLAT_DIR, OUTPUT_DIR, group_field, denormalised)
+    
+    tier1_files = select_zip_files(xlsx_files, denormalised)
+    output_filename = f"{OUTPUT_DIR}/{bionetwork}_denormalised_tier1.zip" if denormalised \
+                 else f"{OUTPUT_DIR}/{bionetwork}_tier1.zip"
+    make_zipfile(tier1_files, output_filename)
 
 if __name__ == '__main__':
     args = define_parser().parse_args()
